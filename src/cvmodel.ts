dc6048bbcf67a2d7e0f032a04ba2e608017f0ac8
SalvaChLl <salvachll@gmail.com>
2021-11-10 13:22:45 +0100

    Adaptando el proyecto

---
+++ src/cvmodel.ts
@@ -0,0 +1,21 @@
+// Converted with https://csvjson.com/csv2json
+
+export interface BibliotecaCV {
+  COD_PROVINCIA: number | string; // 46 | "03",
+  NOM_PROVINCIA: string // "VALENCIA",
+  COD_MUNICIPIO: number; // 228,
+  NOM_MUNICIPIO: string; // "SERRA",
+  TIPO: string // "BIBLIOTECA ESPECIALIZADA",
+  NOMBRE: string; // "BIBLIOTECA ESPECIALIZADA HOSPITAL DR  MOLINER SERRA",
+  DIRECCION: string; // "CALLE PORTA-COELI Nº S/N",
+  CP: number | string; // 46118 | "03005",
+  TELEFONO: string; // "TELF.961687912",
+  FAX: string; // "FAX.963614407",
+  WEB: string; // "HTTP://WWW.COIAL.ORG/",
+  CATALOGO: string; // "HTTP://WWW.BEGV.GVA.ES/IVAJGV/TIVAJ.HTML",
+  EMAIL: string; // "BIBLIOTECA_IVAJ@GVA.ES",
+  CENTRAL: string // "N",
+  COD_CARACTER: string // "PU",
+  DESC_CARACTER: string // "PÚBLICA",
+  DECRETO: string // "N"
+}
